a58fd7a220449afbf88fcc6b6f6b7944f8098cdb
AnjaliThampi02 <139063575+AnjaliThampi02@users.noreply.github.com>
2023-07-09 21:15:12 +0530

    Added a file
    Assignment - 1

---
+++ ASSIGNMENT -1 (My Captain Python - July)  -  ANJALI S THAMPI.py
@@ -0,0 +1,30 @@
+
+# coding: utf-8
+
+# ASSIGNMENT - 1 
+
+# 1. Write a python program which accepts the radius of a circle from the user and computes area.
+
+# In[12]:
+
+
+import math
+
+
+# In[13]:
+
+
+r=float(input("Radius of the circle is: "))
+area=float(math.pi*r**2)
+print(area)
+
+
+# 2. Write a python program to accept a filename from the user and print the extension of that.
+
+# In[16]:
+
+
+file_name=input("Enter the filename: ")
+file_extension=file_name.split(".")
+print("Extension of file is: " + repr(file_extension[-1]))
+
