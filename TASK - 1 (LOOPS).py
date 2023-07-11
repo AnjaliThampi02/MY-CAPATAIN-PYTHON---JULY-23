846734bc2cc78c8953085641ac3ca3128ff28034
AnjaliThampi02 <139063575+AnjaliThampi02@users.noreply.github.com>
2023-07-11 18:25:59 +0530

    Add files via upload

---
+++ TASK - 1 (LOOPS).py
@@ -0,0 +1,30 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+# TASK - 1
+
+
+# In[2]:
+
+
+#  WRITE A PYTHON PROGRAM FOR FIBONACCI NUMBERS.
+
+
+# In[3]:
+
+
+n = 0
+p = 0
+print(p)
+c=1
+print(c)
+while n<60:
+    n=p+c
+    if n<60:
+        print(n)
+    p=c
+    c=n
+
